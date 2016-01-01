e38aa071ffe8df25c6d744bc1b6dfb2de34f0332
jaked122 <jaked122@gmail.com>
2016-01-01 14:17:11 -0500

    Fixed syntax error

---
+++ install_dep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -e cmake-3.4.1 ];then
+    wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.1.tar.gz&&tar xf cmake-3.4.1.tar.gz
+    cd cmake-3.4.1
+    cmake .
+    make -j 4;
+fi;
+sudo make install
+cd ..
+
+if [ ! -e SFML ];then
+    git clone https://github.com/SFML/SFML
+    cd SFML
+    cmake .
+    make -j 4
+fi
+sudo make install
