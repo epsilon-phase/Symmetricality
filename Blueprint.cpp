c82f9f0e93c054f92064b830463ca2a577492243
jaked122 <jaked122@gmail.com>
2016-01-07 21:52:05 -0500

    Fixed floating point error that was more persistant than expected

--- Blueprint.cpp
+++ Blueprint.cpp
@@ -158,6 +158,7 @@ void Blueprint::deserialize(const std::string &file) {
 }
 
 void Blueprint::setDesignation(int x, int y, int z, char d) {
+	_occupation[z].reserve(0);
 	if (d != 'x'&&_occupation.find(z)->second.find(sf::Vector2i(x, y)) == _occupation.find(z)->second.end())
 		this->_Designations[z][sf::Vector2i(x, y)] = d;
 	else
