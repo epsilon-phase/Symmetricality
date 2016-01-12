eb4504ca0e9c6832040e5fce21889ef98dbc5b58
jaked122 <jaked122@gmail.com>
2016-01-12 18:48:17 -0500

    Cleaned up configuration reader by using GetPot::set_prefix

--- Building.cpp
+++ Building.cpp
@@ -51,11 +51,11 @@ void Building::getTexCoords(sf::Vertex *thing) {
 void Building::getAdjustedCoords(int x, int y, int square_size, sf::Vertex *thing) {
     auto f = getSize();
     auto c = getCenter();
-	int x1, x2, y1, y2;
-	x1 = x + render_start.x;
-	y1 = y + render_start.y;
-	x2 = x + render_end.x;
-	y2 = y + render_end.y;
+    int x1, x2, y1, y2;
+    x1 = x + render_start.x;
+    y1 = y + render_start.y;
+    x2 = x + render_end.x;
+    y2 = y + render_end.y;
     thing[0].position.x = square_size * x1;
     thing[0].position.y = square_size * y1;
     thing[1].position.x = square_size * x2;
@@ -69,61 +69,69 @@ void Building::getAdjustedCoords(int x, int y, int square_size, sf::Vertex *thin
 
 Building Building::fromPot(int number, GetPot &conf) {
     std::stringstream z;
-    z << "buildings/" << number << "/" << "Buildingname";
+
+    z << "buildings/" << number << "/";
+    conf.set_prefix(z.str().c_str());
+    z.str("");
+    z << "Buildingname";
+
     std::string bname = conf(z.str().c_str(), "");
-	std::string blocking;
+
+    std::string blocking;
     std::cout << "Building_name:" << bname << std::endl;
     z.str("");
-    z << "buildings/" << number << "/" << "key_sequence";
+    z << "key_sequence";
 
     std::string keySeq = conf(z.str().c_str(), "");
     std::cout << "Key sequence:" << keySeq << std::endl;
     z.str("");
-    z << "buildings/" << number << "/" << "size_x";
+    z << "size_x";
     int sx = conf(z.str().c_str(), 0);
     z.str("");
-    z << "buildings/" << number << "/" << "size_y";
+    z << "size_y";
     int sy = conf(z.str().c_str(), 0);
     z.str("");
-    z << "buildings/" << number << "/" << "center_x";
+    z << "center_x";
     int cx = conf(z.str().c_str(), 0);
     z.str("");
-    z << "buildings/" << number << "/" << "center_y";
+    z << "center_y";
     int cy = conf(z.str().c_str(), 0);
     z.str("");
-	z << "buildings/" << number << "/" << "render_start_x";
-	int rx1 = conf(z.str().c_str(), sx);
-	z.str("");
-	z << "buildings/" << number << "/" << "render_start_y";
-	int ry1 = conf(z.str().c_str(), sy);
-	z.str("");
-	z << "buildings/" << number << "/" << "render_end_x";
-	int rx2 = conf(z.str().c_str(), sx);
-	z.str("");
-	z << "buildings/" << number << "/" << "render_end_y";
-	int ry2 = conf(z.str().c_str(), sy);
-	z.str("");
-    z << "buildings/" << number << "/" << "texturecoords/X1";
+    z << "render_start_x";
+    int rx1 = conf(z.str().c_str(), sx);
+    z.str("");
+    z << "render_start_y";
+    int ry1 = conf(z.str().c_str(), sy);
+    z.str("");
+    z << "render_end_x";
+    int rx2 = conf(z.str().c_str(), sx);
+    z.str("");
+    z << "render_end_y";
+    int ry2 = conf(z.str().c_str(), sy);
+    z.str("");
+    z << "texturecoords/X1";
 
     int tx1 = conf(z.str().c_str(), 0);
     z.str("");
-    z << "buildings/" << number << "/" << "texturecoords/Y1";
+    z << "texturecoords/Y1";
     int ty1 = conf(z.str().c_str(), 0);
     z.str("");
-    z << "buildings/" << number << "/" << "texturecoords/X2";
+    z << "texturecoords/X2";
     int tx2 = conf(z.str().c_str(), 0);
     z.str("");
-    z << "buildings/" << number << "/" << "texturecoords/Y2";
+    z << "texturecoords/Y2";
     int ty2 = conf(z.str().c_str(), 0);
-	z.str();
-	z << "buildings/" << number << "/blocking";
-	blocking = conf(z.str().c_str(),"0");
+    z.str();
+    z << "/blocking";
+    blocking = conf(z.str().c_str(), "0");
 
-    Building f(bname,keySeq,sf::Vector2i(sx,sy),sf::Vector2i(cx,cy),tx1,ty1,tx2,ty2);
-	f.passable.resize(sx*sy);
-	for (int i = 0; i < blocking.size(); i++)
-		f.passable.push_back(blocking[i] == '1' ? true : false);
-	f.render_start = sf::Vector2i(rx1, ry1);
-	f.render_end = sf::Vector2i(rx2, ry2);
-	return f;
+    Building f(bname, keySeq, sf::Vector2i(sx, sy), sf::Vector2i(cx, cy), tx1, ty1, tx2, ty2);
+    f.passable.resize(sx * sy);
+    for (int i = 0; i < blocking.size(); i++)
+        f.passable.push_back(blocking[i] == '1' ? true : false);
+    z << "buildings/" << number << "/";
+    f.render_start = sf::Vector2i(rx1, ry1);
+    f.render_end = sf::Vector2i(rx2, ry2);
+    conf.set_prefix("");
+    return f;
 }
