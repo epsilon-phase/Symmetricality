1375615e3ed888a7b9672da20fb2672ffa9b96d7
jaked122 <jaked122@gmail.com>
2016-02-25 00:38:18 -0500

    Fixed long standing issue with the display not updating when mode is changed
    Fixes #31

--- Hud.cpp
+++ Hud.cpp
@@ -82,8 +82,9 @@ void Hud::update_text() {
     if (renderer.building_mode) {
         f << "Building:" << renderer.current_building->second.getName()<<(renderer.canPlace()?"":" can't build");
         Designation_type.setString(f.str());
-    } else if (old_desig != *renderer.current_designation) {
-
+		wasBuilding = true;
+    } else if (old_desig != *renderer.current_designation||wasBuilding) {
+		wasBuilding = false;
         f << "Designating: " << *renderer.current_designation;
         Designation_type.setString(f.str());
         old_desig = *renderer.current_designation;
